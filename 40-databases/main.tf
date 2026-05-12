d39820dccb09cc5bfc1a7ddc0c75ec7f8c66173c
VivekGuvvala <vivekguvvala7@gmail.com>
2026-05-12 23:05:23 +0530

    hi

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -22,9 +22,15 @@ resource "terraform_data" "mongodb" {
         password = "DevOps321"
         host = aws_instance.mongodb.private_ip
   }
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+
     provisioner "remote-exec" {
         inline = [ 
-        "echo hello!"
+        "sudo sh chmod +x /tmp/bootstrap.sh"
          ]
     }
 }
