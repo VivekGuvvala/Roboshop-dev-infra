b4cdbddadbfb3574ca87f50b69353b439ed11276
VivekGuvvala <vivekguvvala7@gmail.com>
2026-05-17 22:42:30 +0530

    hi

---
+++ 60-catalogue/data.tf
@@ -0,0 +1,17 @@
+data "aws_ami" "joindevops" {
+    owners = ["973714476881"]
+    most_recent = true
+
+    filter {
+      name = "name"
+      values = ["Redhat-9-DevOps-Practice"]
+    }
+}
+
+
+data "aws_ssm_parameter" "catalogue_sg_id" {
+  name = "/${var.project_name}/${var.environment}/catalogue_sg_id"
+}
+data "aws_ssm_parameter" "private_subnet_ids" {
+  name = "/${var.project_name}/${var.environment}/private_subnet_ids"
+}
